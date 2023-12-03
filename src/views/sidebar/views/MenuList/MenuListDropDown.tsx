fe84ee89b2adc359682e9e91ff9467995a3d9476
Shaon07 <alishaon77@gmail.com>
2023-12-03 23:26:03 +0600

    [add] sidebar menu list dropdown component

---
+++ src/views/sidebar/views/MenuList/MenuListDropDown.tsx
@@ -0,0 +1,79 @@
+import SidebarLinkGroup from "@/components/ui/Sidebar/SidebarLinkGroup";
+import { MenuDropDownIcon } from "@/custom-icons";
+import { styles } from "@/styles/tailwind/sidebarViewLayer/index.css";
+import Link from "next/link";
+import React from "react";
+import MenuListItem from "./MenuListItem";
+
+type MenuListDropDownType = {
+  pathname: string;
+  sidebarExpanded: boolean;
+  setSidebarExpanded: (arg: boolean) => void;
+  link: string;
+  value: string;
+  icon: React.ReactNode;
+  title: string;
+  childList: any[];
+};
+
+export default function MenuListDropDown({
+  pathname,
+  sidebarExpanded,
+  setSidebarExpanded,
+  link,
+  value,
+  icon,
+  title,
+  childList,
+}: MenuListDropDownType) {
+  return (
+    <SidebarLinkGroup
+      activeCondition={pathname === link || pathname.includes(value)}
+    >
+      {(handleClick, open) => {
+        return (
+          <React.Fragment>
+            <Link
+              href="#"
+              className={` ${styles.sidebarMenuItem} ${
+                (pathname === link || pathname.includes(value)) &&
+                "bg-graydark dark:bg-meta-4"
+              }`}
+              onClick={(e) => {
+                e.preventDefault();
+                sidebarExpanded ? handleClick() : setSidebarExpanded(true);
+              }}
+            >
+              {icon}
+              {title}
+              <MenuDropDownIcon open={open} />
+            </Link>
+            {/* <!-- Dropdown Menu Start --> */}
+            <div
+              className={`${styles.dropDownMenuWrapper} ${!open && "hidden"}`}
+            >
+              {childList.map((child, index) => {
+                return (
+                  <ul
+                    key={index}
+                    className={`${styles.dropDownMenuListWrapper}`}
+                  >
+                    <MenuListItem
+                      key={index}
+                      href={child.link}
+                      icon={child.icon}
+                      title={child.title}
+                      value={child.value}
+                      pathname={pathname}
+                    />
+                  </ul>
+                );
+              })}
+            </div>
+            {/* <!-- Dropdown Menu End --> */}
+          </React.Fragment>
+        );
+      }}
+    </SidebarLinkGroup>
+  );
+}
--- src/views/sidebar/views/MenuList/index.tsx
+++ src/views/sidebar/views/MenuList/index.tsx
@@ -1,10 +1,7 @@
-import SidebarLinkGroup from "@/components/ui/Sidebar/SidebarLinkGroup";
-import { MenuDropDownIcon } from "@/custom-icons";
 import { checkArray } from "@/helpers";
 import { styles } from "@/styles/tailwind/sidebarViewLayer/index.css";
-import Link from "next/link";
-import React from "react";
 import { menuList } from "../../data/menuList";
+import MenuListDropDown from "./MenuListDropDown";
 import MenuListItem from "./MenuListItem";
 
 type MenuListType = {
@@ -35,63 +32,17 @@ export default function MenuList({
                   item.menuItems.map((item, index) => {
                     if (checkArray(item.children)) {
                       return (
-                        <SidebarLinkGroup
+                        <MenuListDropDown
                           key={index}
-                          activeCondition={
-                            pathname === item.link ||
-                            pathname.includes(item.value)
-                          }
-                        >
-                          {(handleClick, open) => {
-                            return (
-                              <React.Fragment>
-                                <Link
-                                  href="#"
-                                  className={` ${styles.sidebarMenuItem} ${
-                                    (pathname === item.link ||
-                                      pathname.includes(item.value)) &&
-                                    "bg-graydark dark:bg-meta-4"
-                                  }`}
-                                  onClick={(e) => {
-                                    e.preventDefault();
-                                    sidebarExpanded
-                                      ? handleClick()
-                                      : setSidebarExpanded(true);
-                                  }}
-                                >
-                                  {item.icon}
-                                  {item.title}
-                                  <MenuDropDownIcon open={open} />
-                                </Link>
-                                {/* <!-- Dropdown Menu Start --> */}
-                                <div
-                                  className={`${styles.dropDownMenuWrapper} ${
-                                    !open && "hidden"
-                                  }`}
-                                >
-                                  {item.children.map((child, index) => {
-                                    return (
-                                      <ul
-                                        key={index}
-                                        className={`${styles.dropDownMenuListWrapper}`}
-                                      >
-                                        <MenuListItem
-                                          key={index}
-                                          href={child.link}
-                                          icon={child.icon}
-                                          title={child.title}
-                                          value={child.value}
-                                          pathname={pathname}
-                                        />
-                                      </ul>
-                                    );
-                                  })}
-                                </div>
-                                {/* <!-- Dropdown Menu End --> */}
-                              </React.Fragment>
-                            );
-                          }}
-                        </SidebarLinkGroup>
+                          childList={item.children}
+                          icon={item.icon}
+                          title={item.title}
+                          value={item.value}
+                          link={item.link}
+                          pathname={pathname}
+                          sidebarExpanded={sidebarExpanded}
+                          setSidebarExpanded={setSidebarExpanded}
+                        />
                       );
                     } else {
                       return (
