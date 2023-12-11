536d35f79bf30a8366e906245a993dc01c8ddd43
Shaon07 <alishaon77@gmail.com>
2023-12-11 23:36:50 +0600

    [add] channel data view layer and tailwind classnames and data

--- src/components/shareable/ChannelListTable/channelListViewLayer.tsx
+++ src/components/shareable/ChannelListTable/channelListViewLayer.tsx
@@ -32,34 +32,34 @@ export default function ChannelListViewLayer({ channelListData }: ChannelListVie
 
                 {checkArray(channelListData) && channelListData.map((brand, key) => (
                     <div
-                        className={`grid grid-cols-3 sm:grid-cols-5 ${key === channelListData.length - 1
+                        className={`${styles.channelListTableWrapper} ${key === channelListData.length - 1
                             ? ""
-                            : "border-b border-stroke dark:border-strokedark"
+                            : styles.tableListBorder
                             }`}
                         key={key}
                     >
-                        <div className="flex items-center gap-3 p-2.5 xl:p-5">
+                        <div className={`${styles.logoWrapper}`}>
                             <div className="flex-shrink-0">
                                 <Image src={brand.logo} alt="Brand" width={48} height={48} />
                             </div>
-                            <p className="hidden text-black dark:text-white sm:block">
+                            <p className={`${styles.brandName}`}>
                                 {brand.name}
                             </p>
                         </div>
 
-                        <div className="flex items-center justify-center p-2.5 xl:p-5">
+                        <div className={`${styles.listWrapperOne}`}>
                             <p className="text-black dark:text-white">{brand.visitors}K</p>
                         </div>
 
-                        <div className="flex items-center justify-center p-2.5 xl:p-5">
+                        <div className={`${styles.listWrapperOne}`}>
                             <p className="text-meta-3">${brand.revenues}</p>
                         </div>
 
-                        <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
+                        <div className={`${styles.listWrapperTwo}`}>
                             <p className="text-black dark:text-white">{brand.sales}</p>
                         </div>
 
-                        <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
+                        <div className={`${styles.listWrapperTwo}`}>
                             <p className="text-meta-5">{brand.conversion}%</p>
                         </div>
                     </div>
--- src/styles/tailwind/ChannelListViewLayer/index.css.ts
+++ src/styles/tailwind/ChannelListViewLayer/index.css.ts
@@ -3,4 +3,11 @@ export const styles = {
   heading: `mb-6 text-xl font-semibold text-black dark:text-white`,
   channelContentWrapper: `flex flex-col`,
   channelContent: `grid grid-cols-3 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-5`,
+  channelListTableWrapper: `grid grid-cols-3 sm:grid-cols-5`,
+  tableListBorder: `border-b border-stroke dark:border-strokedark`,
+  logoWrapper: `flex items-center gap-3 p-2.5 xl:p-5`,
+  brandName: `hidden text-black dark:text-white sm:block`,
+  listWrapperOne: `flex items-center justify-center p-2.5 xl:p-5`,
+  listWrapperTwo: `hidden items-center justify-center p-2.5 sm:flex xl:p-5
+  hidden items-center justify-center p-2.5 sm:flex xl:p-5`,
 };
