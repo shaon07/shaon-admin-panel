a901c64765d5f662e651026b54e6f679693e7268
Shaon07 <alishaon77@gmail.com>
2023-12-12 00:02:36 +0600

    [add] chat list component view layer, data, types and tailwind classnames

--- src/components/shareable/ChatList/ChatListViewLayer.tsx
+++ src/components/shareable/ChatList/ChatListViewLayer.tsx
@@ -1,4 +1,5 @@
 import { checkArray } from '@/helpers'
+import { styles } from '@/styles/tailwind/ChatListViewLayer/index.css'
 import { chatListType } from '@/types'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -9,8 +10,8 @@ export type ChatListViewLayerProps = {
 
 export default function ChatListViewLayer({ chatList }: ChatListViewLayerProps) {
     return (
-        <div className="col-span-12 rounded-sm border border-stroke bg-white py-6 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-4">
-            <h4 className="mb-6 px-7.5 text-xl font-semibold text-black dark:text-white">
+        <div className={`${styles.chatListViewLayerWrapper}`}>
+            <h4 className={`${styles.heading}`}>
                 Chats
             </h4>
 
@@ -18,32 +19,32 @@ export default function ChatListViewLayer({ chatList }: ChatListViewLayerProps) 
                 {checkArray(chatList) && chatList.map((chat, key) => (
                     <Link
                         href="/"
-                        className="flex items-center gap-5 py-3 px-7.5 hover:bg-gray-3 dark:hover:bg-meta-4"
+                        className={`${styles.chatListWrapper}`}
                         key={key}
                     >
-                        <div className="relative h-14 w-14 rounded-full">
+                        <div className={`${styles.chatListContent}`}>
                             <Image src={chat.avatar} alt="User" width={57} height={56} />
                             <span
-                                className={`absolute right-0 bottom-0 h-3.5 w-3.5 rounded-full border-2 border-white ${chat.dot === 6 ? "bg-meta-6" : `bg-meta-${chat.dot}`
+                                className={`${styles.chatListDot} ${chat.dot === 6 ? "bg-meta-6" : `bg-meta-${chat.dot}`
                                     } `}
                             ></span>
                         </div>
 
-                        <div className="flex flex-1 items-center justify-between">
+                        <div className={`${styles.chatListDataWrapper}`}>
                             <div>
-                                <h5 className="font-medium text-black dark:text-white">
+                                <h5 className={`${styles.chatName}`}>
                                     {chat.name}
                                 </h5>
                                 <p>
-                                    <span className="text-sm text-black dark:text-white">
+                                    <span className={`${styles.chatText}`}>
                                         {chat.text}
                                     </span>
                                     <span className="text-xs"> . {chat.time} min</span>
                                 </p>
                             </div>
                             {chat.textCount !== 0 && (
-                                <div className="flex h-6 w-6 items-center justify-center rounded-full bg-primary">
-                                    <span className="text-sm font-medium text-white">
+                                <div className={`${styles.chatCountWrapper}`}>
+                                    <span className={`${styles.chatCount}`}>
                                         {" "}
                                         {chat.textCount}
                                     </span>
---
+++ src/styles/tailwind/ChatListViewLayer/index.css.ts
@@ -0,0 +1,13 @@
+export const styles = {
+  chatListViewLayerWrapper: `col-span-12 rounded-sm border border-stroke bg-white py-6 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-4`,
+  heading: `mb-6 px-7.5 text-xl font-semibold text-black dark:text-white`,
+  chatListWrapper: `flex items-center gap-5 py-3 px-7.5 hover:bg-gray-3 dark:hover:bg-meta-4`,
+  chatListContent: `relative h-14 w-14 rounded-full`,
+  contentWrapper: `flex w-full items-center`,
+  chatListDot: `absolute right-0 bottom-0 h-3.5 w-3.5 rounded-full border-2 border-white`,
+  chatListDataWrapper: `flex flex-1 items-center justify-between`,
+  chatName: `font-medium text-black dark:text-white`,
+  chatText: `text-sm text-black dark:text-white`,
+  chatCountWrapper: `flex h-6 w-6 items-center justify-center rounded-full bg-primary`,
+  chatCount: `text-sm font-medium text-white`,
+};
