ab2a98cbe11ef340ec8baadf657031c72f2a1f0f
Shaon07 <alishaon77@gmail.com>
2023-12-14 21:04:35 +0600

    [add] loader in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 "use client";
 
+import Loader from "@/components/shareable/Loader";
 import Header from "@/components/ui/Header";
 import Sidebar from "@/components/ui/Sidebar";
 import { Inter } from "next/font/google";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,32 +14,39 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
 
   return (
     <html lang="en">
       <body suppressHydrationWarning={true}>
         <div className="dark:bg-boxdark-2 dark:text-bodydark">
-          <div className="flex h-screen overflow-hidden">
-            {/* <!-- ===== Sidebar Start ===== --> */}
-            <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-            {/* <!-- ===== Sidebar End ===== --> */}
+          {
+            loading ? <Loader /> : <div className="flex h-screen overflow-hidden">
+              {/* <!-- ===== Sidebar Start ===== --> */}
+              <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+              {/* <!-- ===== Sidebar End ===== --> */}
 
-            {/* <!-- ===== Content Area Start ===== --> */}
-            <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-              {/* <!-- ===== Header Start ===== --> */}
-              <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-              {/* <!-- ===== Header End ===== --> */}
+              {/* <!-- ===== Content Area Start ===== --> */}
+              <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+                {/* <!-- ===== Header Start ===== --> */}
+                <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+                {/* <!-- ===== Header End ===== --> */}
 
-              {/* <!-- ===== Main Content Start ===== --> */}
-              <main>
-                <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-                  {children}
-                </div>
-              </main>
-              {/* <!-- ===== Main Content End ===== --> */}
+                {/* <!-- ===== Main Content Start ===== --> */}
+                <main>
+                  <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                    {children}
+                  </div>
+                </main>
+                {/* <!-- ===== Main Content End ===== --> */}
+              </div>
+              {/* <!-- ===== Content Area End ===== --> */}
             </div>
-            {/* <!-- ===== Content Area End ===== --> */}
-          </div>
+          }
         </div>
       </body>
     </html>
