af600cc754ec11490b70fc312c28d2eeab19aa0d
Shaon07 <alishaon77@gmail.com>
2023-12-16 23:23:59 +0600

    [add] switcher one tailwind classnames

---
+++ src/styles/tailwind/Switcher/one.css.ts
@@ -0,0 +1,6 @@
+export const styles = {
+  label: `flex cursor-pointer select-none items-center`,
+  content: `block h-8 w-14 rounded-full bg-meta-9 dark:bg-[#5A616B]`,
+  box: `absolute left-1 top-1 h-6 w-6 rounded-full bg-white transition`,
+  enable: `!right-1 !translate-x-full !bg-primary dark:!bg-white`,
+};
