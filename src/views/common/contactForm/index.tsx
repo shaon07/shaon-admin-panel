c46d98faf9d208437cfd99bf375537d232e417cc
Shaon07 <alishaon77@gmail.com>
2023-12-29 00:03:56 +0600

    [add] selectbox design

--- src/views/common/contactForm/index.tsx
+++ src/views/common/contactForm/index.tsx
@@ -1,4 +1,7 @@
+
+"use client";
 import Button from "@/components/shareable/Button";
+import SelectBox from "@/components/shareable/SelectBox";
 import FormInputBox from "@/components/shareable/forms/FormInputBox";
 import FormTextAreaBox from "@/components/shareable/forms/FormTextArea";
 import { styles } from "@/styles/tailwind/contactForm/index.css";
@@ -35,36 +38,7 @@ export default function ContactForm() {
           </div>
 
           <div className="mb-4.5">
-            <label className="mb-2.5 block text-black dark:text-white">
-              Subject
-            </label>
-            <div className="relative z-20 bg-transparent dark:bg-form-input">
-              <select className="relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-5 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary">
-                <option value="">Type your subject</option>
-                <option value="">USA</option>
-                <option value="">UK</option>
-                <option value="">Canada</option>
-              </select>
-              <span className="absolute top-1/2 right-4 z-30 -translate-y-1/2">
-                <svg
-                  className="fill-current"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <g opacity="0.8">
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
-                      fill=""
-                    ></path>
-                  </g>
-                </svg>
-              </span>
-            </div>
+            <SelectBox label="Subject" options={[{ value: '1', label: 'Option 1' }, { value: '2', label: 'Option 2' }]} onChange={(data) => { console.log(data) }} />
           </div>
 
           <div className="mb-6">
