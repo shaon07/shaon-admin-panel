9cabcc68bc14655aeda763384002ac80304eca57
Shaon07 <alishaon77@gmail.com>
2023-12-28 23:05:20 +0600

    [add] contact form refactor

---
+++ src/styles/tailwind/global/index.css.ts
@@ -0,0 +1,4 @@
+export const globalStyles = {
+  container: `rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark`,
+  border: `border-b border-stroke py-4 px-6.5 dark:border-strokedark`,
+};
