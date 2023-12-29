31811bb701b096ae9dac341af07b8c8a30cd7ea8
Shaon07 <alishaon77@gmail.com>
2023-12-29 20:45:31 +0600

    [add] sign-in form refactor

---
+++ src/styles/tailwind/SignInForm/index.css.ts
@@ -0,0 +1,4 @@
+export const styles = {
+  heading: `font-medium text-black dark:text-white`,
+  checkBoxWrapper: `box mr-3 flex h-5 w-5 items-center justify-center rounded border border-stroke dark:border-strokedark`,
+};
