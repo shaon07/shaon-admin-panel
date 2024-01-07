d97b7cb80aaeb75b36171bac92c6bbbbfa593197
Shaon07 <alishaon77@gmail.com>
2024-01-07 20:58:46 +0600

    [add] initial chart page design

---
+++ src/components/pages/chart-page/index.tsx
@@ -0,0 +1,10 @@
+import BreadCrumb from "@/components/ui/pages/calender/breadCrumb";
+
+export default function ChartPageViewLayers() {
+    return (
+        <div>
+            <BreadCrumb title="Chart" />
+            <h1>This is chart page view layers</h1>
+        </div>
+    )
+}
