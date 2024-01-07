a942099bc9b0a857ff61f020b9a3cb85755f1eaf
Shaon07 <alishaon77@gmail.com>
2024-01-07 21:22:22 +0600

    [add] visitor bar chart design

--- src/components/pages/chart-page/index.tsx
+++ src/components/pages/chart-page/index.tsx
@@ -1,10 +1,12 @@
+import BarChart from "@/components/shareable/BarChart";
 import BreadCrumb from "@/components/ui/pages/calender/breadCrumb";
 
 export default function ChartPageViewLayers() {
     return (
         <div>
             <BreadCrumb title="Chart" />
-            <h1>This is chart page view layers</h1>
+            <BarChart />
+            {/* <VisitorChart /> */}
         </div>
     )
 }
